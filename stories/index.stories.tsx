01d94b06efd9f21a1967a243081d6ccac8f50d36
Jack Moseley <jackwmoseley@gmail.com>
2020-03-27 19:56:54 +0000

    Changed transportTypes and route detail example

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 
@@ -12,6 +12,8 @@ import {
   PolylineOverlay,
   Directions,
 } from '../src'
+
+import { RouteDetailExample } from './components.stories'
 import devToken from '../devToken'
 
 const UseMapExample = () => {
@@ -164,7 +166,7 @@ storiesOf('Directions', module)
       <Directions
         origin={[47.6754, -122.2084]}
         destination={[47.68, -122.2135]}
-        transportType="Automobile"
+        transportType={mapkit.Directions.Transport.Automobile}
         style={{ lineWidth: 3 }}
       />
     </Map>
@@ -182,39 +184,9 @@ storiesOf('Directions', module)
       <Directions
         origin={[47.6754, -122.2084]}
         destination={[47.68, -122.2135]}
-        transportType="Walking"
+        transportType={mapkit.Directions.Transport.Automobile}
         style={{ lineWidth: 3 }}
       />
     </Map>
   ))
-  .add('Getting route details', () => {
-    const [time, setTime] = useState(0)
-    const [distance, setDistance] = useState(0)
-    return (
-      <>
-        <p>
-          It will take {time} seconds to walk {distance} meters
-        </p>
-        <Map
-          tokenOrCallback={devToken}
-          region={{
-            latitude: 47.6754,
-            longitude: -122.2084,
-            latitudeSpan: 0.006,
-            longitudeSpan: 0.006,
-          }}
-        >
-          <Directions
-            origin={[47.6754, -122.2084]}
-            destination={[47.68, -122.2135]}
-            transportType="Walking"
-            style={{ lineWidth: 3 }}
-            routeDetails={({ expectedTravelTime, distance }) => {
-              setTime(expectedTravelTime)
-              setDistance(distance)
-            }}
-          />
-        </Map>
-      </>
-    )
-  })
+  .add('Getting route details', () => <RouteDetailExample />)
