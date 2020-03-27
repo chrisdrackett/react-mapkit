0ea73df9f568569dc80b7dd955a4ddf39fa16d16
Jack Moseley <jackwmoseley@gmail.com>
2020-03-27 12:48:43 +0000

    Added story for directions

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { storiesOf } from '@storybook/react'
 
@@ -10,6 +10,7 @@ import {
   CircleOverlay,
   PolygonOverlay,
   PolylineOverlay,
+  Directions,
 } from '../src'
 import devToken from '../devToken'
 
@@ -148,3 +149,72 @@ storiesOf('Overlays', module)
       />
     </Map>
   ))
+
+storiesOf('Directions', module)
+  .add('Driving', () => (
+    <Map
+      tokenOrCallback={devToken}
+      region={{
+        latitude: 47.6754,
+        longitude: -122.2084,
+        latitudeSpan: 0.006,
+        longitudeSpan: 0.006,
+      }}
+    >
+      <Directions
+        origin={[47.6754, -122.2084]}
+        destination={[47.68, -122.2135]}
+        transportType="Automobile"
+        style={{ lineWidth: 3 }}
+      />
+    </Map>
+  ))
+  .add('Walking', () => (
+    <Map
+      tokenOrCallback={devToken}
+      region={{
+        latitude: 47.6754,
+        longitude: -122.2084,
+        latitudeSpan: 0.006,
+        longitudeSpan: 0.006,
+      }}
+    >
+      <Directions
+        origin={[47.6754, -122.2084]}
+        destination={[47.68, -122.2135]}
+        transportType="Walking"
+        style={{ lineWidth: 3 }}
+      />
+    </Map>
+  ))
+  .add('Getting route details', () => {
+    const [time, setTime] = useState(0)
+    const [distance, setDistance] = useState(0)
+    return (
+      <>
+        <p>
+          It will take {time} seconds to walk {distance} meters
+        </p>
+        <Map
+          tokenOrCallback={devToken}
+          region={{
+            latitude: 47.6754,
+            longitude: -122.2084,
+            latitudeSpan: 0.006,
+            longitudeSpan: 0.006,
+          }}
+        >
+          <Directions
+            origin={[47.6754, -122.2084]}
+            destination={[47.68, -122.2135]}
+            transportType="Walking"
+            style={{ lineWidth: 3 }}
+            routeDetails={({ expectedTravelTime, distance }) => {
+              setTime(expectedTravelTime)
+              setDistance(distance)
+            }}
+          />
+        </Map>
+      </>
+    )
+  })
